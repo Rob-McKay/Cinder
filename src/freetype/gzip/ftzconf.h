5b7b65da630f563bcd9e6b991319b619864f64d9
Rob McKay <16419309+Rob-McKay@users.noreply.github.com>
2025-10-24 15:12:13 +0100

    Fixed missing Byte definition

--- src/freetype/gzip/ftzconf.h
+++ src/freetype/gzip/ftzconf.h
@@ -217,6 +217,8 @@
 
 #if !defined(MACOS) && !defined(TARGET_OS_MAC)
 typedef unsigned char  Byte;  /* 8 bits */
+#else
+typedef uint8_t Byte;
 #endif
 typedef unsigned int   uInt;  /* 16 bits or more */
 typedef unsigned long  uLong; /* 32 bits or more */
